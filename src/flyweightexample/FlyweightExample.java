271fe902a61df96e3808fa7a3425168a00401a86
Christopher Kilian <cdkilian@gmail.com>
2017-11-24 12:57:17 -0800

    Initialize

--- src/flyweightexample/FlyweightExample.java
+++ src/flyweightexample/FlyweightExample.java
@@ -15,7 +15,7 @@ public class FlyweightExample {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // Changed a thing
     }
     
 }
